5cff5082c836fe6a2117f974444dc056d1cc43b6
Oskar Dahlström <0skar01@hotmail.se>
2025-05-21 10:08:36 +0200

    SwedishPuns

---
+++ swedishPuns/src/main/java/com/example/swedishpuns/SwedishPunsApplication.java
@@ -0,0 +1,32 @@
+package com.example.swedishpuns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@SpringBootApplication
+public class SwedishPunsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwedishPunsApplication.class, args);
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/secure").hasAuthority("SCOPE_read_resource") // Or just .authenticated()
+                        .requestMatchers("/public").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+}
